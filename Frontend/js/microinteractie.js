390a0b06c0942460a0d145189e7f73b34ae909be
pcsart <35337742+pcsart@users.noreply.github.com>
2019-08-19 17:40:34 +0200

    Add files via upload

--- Frontend/js/microinteractie.js
+++ Frontend/js/microinteractie.js
@@ -9,3 +9,41 @@ function verander_naar_vinkje() {
 	togglebutton.classList.toggle('opgeslagenverhaal');
 }
 opslaanbutton.addEventListener('click', verander_naar_vinkje);
+
+// likes bovenin
+var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+
+
+
+// reageer
+////https://codepen.io/phcacique/pen/BzoLLY
+var pop, 	reageerbutton, close, mainpopup;
+var isOpen = false;
+
+window.onload = function(){
+	pop = document.getElementById("popup");	
+	pop.style.display = "none";
+	
+	reageerbutton = document.getElementById("reageerbutton");
+	
+	close = document.getElementById("closing");
+	
+		reageerbutton.addEventListener("click", function(){
+		pop.style.display = "block";
+		mainpopup.style.left = document.body.clientWidth/2 - mainpopup.offsetWidth/2 + "px";
+		mainpopup.style.top = document.body.clientHeight/2 - mainpopup.offsetHeight/2 + "px";
+		
+	});
+	
+	close.addEventListener("click", function(){
+		pop.style.display = "none";
+	});
+	
+	mainpopup = document.getElementById("mainpopup");
+	
+	
+}
